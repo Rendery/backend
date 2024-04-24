058fd67b0d21bf367cadcc712c6303cd25797d61
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2024-04-24 16:28:43 -0500

    feat(properties): added production connection string.

--- src/main/java/easyinventory/backend/BackendApplication.java
+++ src/main/java/easyinventory/backend/BackendApplication.java
@@ -8,6 +8,8 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		String url = "http://localhost:8080/swagger-ui.html";
+		System.out.println("\n• Swagger UI is available at » " + url);
 	}
 
 }
